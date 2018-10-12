fc988cb926f39cb6a180cc0aa994e0435759f525
VladLungul <vladlungul@gmail.com>
2018-10-12 19:07:13 +0300

    models

---
+++ rentsite/rental/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Car(models.Model):
+    car_id = models.AutoField(primary_key=True)
+    manufacturer = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
+    grade = models.CharField(max_length=25)
+    year = models.CharField(max_length=4)
+    fuel_type =
+
+
+class Owner(models.Model):
+
+
+class RegisteredUser(models.Model):
+
+
+class UnregisteredUser(models.Model):
+
+
