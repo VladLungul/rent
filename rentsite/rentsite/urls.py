550f8b91e98a494f4dac9b81a47bb116af614c38
pbdgk <gordonworkspace@gmail.com>
2018-11-23 16:36:12 +0200

    added url entry for mainapp

--- rentsite/rentsite/urls.py
+++ rentsite/rentsite/urls.py
@@ -3,4 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('authenticate/', include('authentication_app.urls')),
+    path('', include('mainapp.urls')),
 ]
