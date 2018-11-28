f9cff80c9b92e3e9067ac2ba334f895776bc0227
pbdgk <gordonworkspace@gmail.com>
2018-11-28 16:02:12 +0200

    created base profile app

---
+++ rentsite/userprofile/views.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
+from django.urls import reverse_lazy
+from django.shortcuts import render
+from django.shortcuts import redirect
+
+from .forms import ProfileForm
+
+
+class ProfileView(LoginRequiredMixin, View):
+    login_url = reverse_lazy('login')
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request):
+        profile = request.user.userprofile
+        form = ProfileForm(instance=profile)
+        return render(request, 'userprofile/profile.html', context={'form': form})
+    
+    def post(self, request):
+        profile = request.user.userprofile
+        form = ProfileForm(request.POST, request.FILES or None, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('profile'))
+        return render(request, 'userprofile/profile.html', context={'form': form})
+
+        
+
