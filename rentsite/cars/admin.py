8a5ecc901f3418587ba4e8de2e27b028ed32f59b
pbdgk <gordonworkspace@gmail.com>
2018-11-28 16:03:20 +0200

    started to create car app

--- rentsite/cars/admin.py
+++ rentsite/cars/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Car, Rent_type
+from .models import Car
 
-class CarAdmin(admin.ModelAdmin):
-    list_display = ('car_id', 'manufacturer', 'model', 'grade', 'year', 'fuel_type', 'engine_capacity',
-                    'car_class', 'rent_type', 'price', 'discount', 'active', 'create_date',
-                    'update_date', 'city')
-    ordering = ('car_id', 'rent_type')
-    list_display_links = list_display
+# class CarAdmin(admin.ModelAdmin):
+#     list_display = ('car_id', 'manufacturer', 'model', 'grade', 'year', 'fuel_type', 'engine_capacity',
+#                     'car_class', 'rent_type', 'price', 'discount', 'active', 'create_date',
+#                     'update_date', 'city')
+#     ordering = ('car_id', 'rent_type')
+#     list_display_links = list_display
 
-class RenttypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    list_display_links = list_display
+# class RenttypeAdmin(admin.ModelAdmin):
+#     list_display = ('name',)
+#     list_display_links = list_display
 
-admin.site.register(Car, CarAdmin)
-admin.site.register(Rent_type, RenttypeAdmin)
+admin.site.register(Car)
+# admin.site.register(Rent_type, RenttypeAdmin)
