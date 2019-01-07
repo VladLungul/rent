dad6534adde3170d7e2075ba81a19983b9d723d6
pbdgk <gordonworkspace@gmail.com>
2019-01-07 20:32:11 +0200

    base pages created

--- rentsite/mainapp/urls.py
+++ rentsite/mainapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 
+app_name = 'mainapp'
+
+
 urlpatterns = [
-    path('home', views.home, name='index')
+    path('', views.RentType.as_view(), name='index'),
+    path('<str:rent_type>/', views.CarClass.as_view(), name='car_class'),
+    path('<str:rent_type>/<str:car_class>/', views.CarList.as_view(), name='car-list'),
 ]
--- rentsite/mainapp/views.py
+++ rentsite/mainapp/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render
+from django.views import View
+
+from cars.choice import CAR_CLASS, RENT_TYPE
 
 
-def home(request):
-    return render(request, 'mainapp/index.html')
+RENT_TYPE_SLUG_MAP = {'pochasovaya': 'Hourly',
+                      'posutochnaya': 'Daily',
+                      'dolgosrochnaya': 'Longterm',
+                      }
+
+
+CAR_CLASS_SLUG_MAP = {'econom-class': 'Econom',
+                      'sredniy-class': 'Middle',
+                      'biznes-class': 'Business',
+                      'premium-class': 'Premium'
+                      }
+
+
+class RentType(View):
+    template_name = 'mainapp/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class CarClass(View):
+    template_name = 'mainapp/car_class.html'
+
+    def get(self, request, rent_type):
+
+        if rent_type not in RENT_TYPE_SLUG_MAP:
+            return render(request, 'mainapp/page404.html')
+        return render(request, self.template_name)
+
+
+class CarList(View):
+    template_name = 'mainapp/car_list.html'
+
+    def get(self, request, rent_type, car_class):
+        if rent_type not in RENT_TYPE_SLUG_MAP or car_class not in CAR_CLASS_SLUG_MAP:
+            return render(request, 'mainapp/page404.html')
+        # cars = Car.ojects.filter(rent_type=rent_type, car_class=car_class)
+        context = {'cars': {}}
+        return render(request, self.template_name, context=context)
